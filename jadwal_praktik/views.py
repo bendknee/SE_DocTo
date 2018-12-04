6017c05b3c6a5b4e6e67ca6c20a6f650c978e7d9
Marshal Arijona Sinaga <arijonamarshal@gmail.com>
2018-12-04 21:53:06 +0700

    tahap1

--- jadwal_praktik/views.py
+++ jadwal_praktik/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     response = {}
-    return render(request,'base.html',response)
+    return render(request,'kelola_jadwal.html',response)
