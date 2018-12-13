47c454416fd75e6f2f571b2dedbe88bd21ff9dd1
Ezra Rizkiatama <erizkiatama@gmail.com>
2018-12-13 10:51:47 +0700

    Fixing every error

--- patient_list/urls.py
+++ patient_list/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	path('', views.showList, name="patient_list"),
 	path('<int:pk>/detail/', views.showDetail, name="patient_detail"),
 	path('order/', views.orderBy, name="order_by"),
-	path('delete/<int:consultation_pk>/', views.delete, name='delete-consul'),
+	path('delete/<int:pk>/', views.delete, name='delete-consul'),
 ]
